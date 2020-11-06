73c2657a93904d90e85e7df95c398962c37e9d14
shahzaintariq.crpyto@gmail.com <shahzaintariq.crpyto@gmail.com>
2020-11-07 03:20:56 +0500

    solve 2 challenge

---
+++ evenOdd/evenOdd.js
@@ -0,0 +1,13 @@
+/*
+EVEN ODD CHALLENGE
+*/
+
+function evenOdd(num){
+    if(num % 2 === 0){
+        return 'EVEN';
+    }else{
+        return 'ODD';
+    }
+}
+
+console.log(evenOdd(8))
---
+++ oppositeNumber/oppositeNumber.js
@@ -0,0 +1,9 @@
+/* 
+VERY SIMPLE, GIVEN A NUMBER FIND ITS OPPOSITE
+*/
+
+function oppositeNum(num){
+    return -num;
+}
+
+console.log(oppositeNum(900))// -900
